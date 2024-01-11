afbe30e63cd91df6916596c6ad5f4e232edc09c4
Teylor Chapman <5535@holbertonstudents.com>
2024-01-10 20:44:35 -0600

    it is what it is

--- 0x0E-unity-webxr/Assets/Scripts/CameraController.cs
+++ 0x0E-unity-webxr/Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float speed = 5;
     public float verticalSpeed = 1.5f;
     public float horizontalSpeed = 1.5f;
     private float xRotation = 0f;
@@ -11,11 +12,22 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = transform.localPosition;
+            position.z += Time.deltaTime * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = transform.localPosition;
+            position.z -= Time.deltaTime * speed;
+        }
         float mouseX = Input.GetAxis("Mouse X") * verticalSpeed * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * horizontalSpeed * Time.deltaTime;
 
         xRotation += mouseX;
-        yRotation += mouseY;
+        yRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         yRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(yRotation, xRotation, 0f);
